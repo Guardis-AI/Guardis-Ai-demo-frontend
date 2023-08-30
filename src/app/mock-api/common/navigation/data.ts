9dfefc8d405d899705b165d6387f8f8d14a1a40f
Peng Hao Lu <pengluh@gmail.com>
2023-08-30 16:37:22 -0700

    separated playback and live

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -1,108 +1,92 @@
 /* tslint:disable:max-line-length */
 /* tslint:disable:max-line-length */
-import { FuseNavigationItem } from '@fuse/components/navigation';
+import { FuseNavigationItem } from "@fuse/components/navigation";
 
-export const defaultNavigation: FuseNavigationItem[] = [
-
- ];
+export const defaultNavigation: FuseNavigationItem[] = [];
 export const compactNavigation: FuseNavigationItem[] = [
-   {
-    id   : 'Setup',
-   title: 'Setup',
+  {
+    id: "Setup",
+    title: "Setup",
     // tooltip : 'Setup',
-    type    : 'aside',
-    icon : 'heroicons_outline:cog',
+    type: "aside",
+    icon: "heroicons_outline:cog",
     children: [
-        {
-            id   : 'user',
-            title: 'User',
-            type : 'basic',
-            icon : 'heroicons_outline:user',
-            link : 'user',
-        },
-     ]
-   },
-   {
-    id   : 'Play',
-    title: 'Play',
-    type : 'aside',
-    icon : 'heroicons_outline:play',
-    children: [
-        {
-            id   : 'Playback',
-            title: 'Playback',
-            type : 'basic',
-            icon : 'heroicons_outline:play',
-            link : '/playback',
-        },
-        {
-            id   : 'Live',
-            title: 'Live',
-            type : 'basic',
-            icon : 'heroicons_outline:play',
-            link : '/live',
-        },
-
-     ]
-   },
-
-    {
-        id      : 'notifications',
-        title   : 'Notification',
-        // tooltip : 'Notification',
-        type    : 'basic',
-        icon    : 'heroicons_outline:bell',
-        link : '/viewnotifications',
-     },
+      {
+        id: "user",
+        title: "User",
+        type: "basic",
+        icon: "heroicons_outline:user",
+        link: "user",
+      },
+    ],
+  },
+  {
+    id: "Live",
+    title: "Live",
+    type: "basic",
+    icon: "heroicons_outline:video-camera",
+    link: "/live",
+  },
+  {
+    id: "Playback",
+    title: "Playback",
+    type: "basic",
+    icon: "heroicons_outline:play",
+    link: "/playback",
+  },
+  {
+    id: "notifications",
+    title: "Notification",
+    // tooltip : 'Notification',
+    type: "basic",
+    icon: "heroicons_outline:bell",
+    link: "/viewnotifications",
+  },
 ];
-export const futuristicNavigation: FuseNavigationItem[] = [
-    
-];
+export const futuristicNavigation: FuseNavigationItem[] = [];
 export const horizontalNavigation: FuseNavigationItem[] = [
-    {
-        id   : 'Setup',
-        title: 'Setup',
-        type    : 'collapsable',
-        link : '/setup',
-        icon : 'heroicons_outline:cog',
-        children: [
-            {
-                id   : 'user',
-                title: 'User',
-                type : 'basic',
-                icon : 'heroicons_outline:user',
-                link : 'user',
-            },
-           
-        ]
-    },
-    {
-        id   : 'Playback',
-        title: 'Playback',
-        type : 'basic',
-        icon : 'heroicons_outline:play',
-        link : '/playback'
-    },
-    {
-        id   : 'Live',
-        title: 'Live',
-        type : 'basic',
-        icon : 'heroicons_outline:video-camera',
-        link : '/live'
-    },
-    {
-        id   : 'Notification',
-        title: 'Notification',
-        type : 'basic',
-        icon : 'heroicons_outline:bell',
-        link : '/viewnotifications'
-    },
-    // {
-    //     id   : 'Setting',
-    //     title: 'Setting',
-    //     type : 'basic',
-    //     icon : 'heroicons_outline:cog',
-    //     link : '/setting'
-    // }
-    
+  {
+    id: "Setup",
+    title: "Setup",
+    type: "collapsable",
+    link: "/setup",
+    icon: "heroicons_outline:cog",
+    children: [
+      {
+        id: "user",
+        title: "User",
+        type: "basic",
+        icon: "heroicons_outline:user",
+        link: "user",
+      },
+    ],
+  },
+  {
+    id: "Playback",
+    title: "Playback",
+    type: "basic",
+    icon: "heroicons_outline:play",
+    link: "/playback",
+  },
+  {
+    id: "Live",
+    title: "Live",
+    type: "basic",
+    icon: "heroicons_outline:video-camera",
+    link: "/live",
+  },
+  {
+    id: "Notification",
+    title: "Notification",
+    type: "basic",
+    icon: "heroicons_outline:bell",
+    link: "/viewnotifications",
+  },
+  // {
+  //     id   : 'Setting',
+  //     title: 'Setting',
+  //     type : 'basic',
+  //     icon : 'heroicons_outline:cog',
+  //     link : '/setting'
+  // }
 ];
--- src/app/modules/admin/livestream/livestream.component.ts
+++ src/app/modules/admin/livestream/livestream.component.ts
@@ -1,91 +1,106 @@
-import { Component, ElementRef, Input, OnInit, ChangeDetectionStrategy , AfterViewInit} from '@angular/core';
-import { StreamService } from 'app/services/stream.service';
-import { DatePipe } from '@angular/common';
-import Hls from 'hls.js';
-import {FormControl} from '@angular/forms';
-import { DomSanitizer } from '@angular/platform-browser';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ChangeDetectionStrategy,
+  AfterViewInit,
+} from "@angular/core";
+import { StreamService } from "app/services/stream.service";
+import { DatePipe } from "@angular/common";
+import Hls from "hls.js";
+import { FormControl } from "@angular/forms";
+import { DomSanitizer } from "@angular/platform-browser";
 declare const videojs: any;
-import { HttpClient } from '@angular/common/http';
- 
- 
+import { HttpClient } from "@angular/common/http";
+
 @Component({
-  selector: 'app-livestream',
-  templateUrl: './livestream.component.html',
-  styleUrls: ['./livestream.component.scss'],
+  selector: "app-livestream",
+  templateUrl: "./livestream.component.html",
+  styleUrls: ["./livestream.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [ {
-    provide: DatePipe
-  },
-],
+  providers: [
+    {
+      provide: DatePipe,
+    },
+  ],
 })
-export class LivestreamComponent implements OnInit,AfterViewInit {
-  date = new FormControl('2023-07-27');
-  cfurl:any= localStorage.getItem('cfUrl');
-  private _elementRef: ElementRef
-  searchText:any;playbacklist :any;stream='';vdata :any;  cameraType:'01';
+export class LivestreamComponent implements OnInit, AfterViewInit {
+  date = new FormControl("2023-07-27");
+  cfurl: any = localStorage.getItem("cfUrl");
+  private _elementRef: ElementRef;
+  searchText: any;
+  playbacklist: any;
+  stream = "";
+  vdata: any;
+  cameraType: "01";
   selectedDate = new Date();
-  dateplay :any;
+  dateplay: any;
   public video: HTMLVideoElement;
- public hls: Hls;
- @Input() max: any;
- toggle = true;
- maxDate: any = new Date();
+  public hls: Hls;
+  @Input() max: any;
+  toggle = true;
+  maxDate: any = new Date();
 
- devicelist = [];
+  devicelist = [];
   // declare player var
-   private player = videojs.getPlayers();
-   constructor(private sanitizer:DomSanitizer,
-   private _StreamService:StreamService,
-   public datepipe: DatePipe,
-   elementRef: ElementRef, 
-   private http: HttpClient,
-   private el: ElementRef<HTMLVideoElement>) {
-   this.player = false;
-   this.video = el.nativeElement;
-      // this.maxDate.setDate(this.maxDate.getDate()-1);
+  private player = videojs.getPlayers();
+  constructor(
+    private sanitizer: DomSanitizer,
+    private _StreamService: StreamService,
+    public datepipe: DatePipe,
+    elementRef: ElementRef,
+    private http: HttpClient,
+    private el: ElementRef<HTMLVideoElement>
+  ) {
+    this.player = false;
+    this.video = el.nativeElement;
+    // this.maxDate.setDate(this.maxDate.getDate()-1);
   }
- 
-  
+
   ngOnInit() {
-    const data = {"p_user_id":localStorage.getItem('userId'),"p_device_token":"","p_device_type":"","p_port":"","login_user_id":localStorage.getItem('userId')}
-     this. _StreamService.getDeviceList(data).subscribe((dv:any)=>{
-      this.devicelist = dv.gai_get_device_list
+    const data = {
+      p_user_id: localStorage.getItem("userId"),
+      p_device_token: "",
+      p_device_type: "",
+      p_port: "",
+      login_user_id: localStorage.getItem("userId"),
+    };
+    this._StreamService.getDeviceList(data).subscribe((dv: any) => {
+      this.devicelist = dv.gai_get_device_list;
       const firstCam = this.devicelist[0].camera_type;
-      this.dateplay =  this.datepipe.transform(this.selectedDate, 'yyyyMMdd');
-      this.getPlayback(this.dateplay,firstCam);
-    })
- 
-  } 
+      this.dateplay = this.datepipe.transform(this.selectedDate, "yyyyMMdd");
+      this.getPlayback(this.dateplay, firstCam);
+    });
+  }
 
-  
-  getPlayback(date,camtype){
-   this.cameraType = camtype;
-      this.toggle = !this.toggle;
-      console.log(date);
-      this.stream = this.cfurl+'media/'+camtype+'/'+date+'/output.m3u8';
-      //this.stream = 'https://guardisai-test-unit-2.bullshouse.in/media/Cashier22/20230803/output.m3u8';
-      console.log(this.stream);
-      this.vdata = {videolink : this.stream}
-      this.player.src({
-        src: this.stream
-      });
-    }
+  getPlayback(date, camtype) {
+    this.cameraType = camtype;
+    this.toggle = !this.toggle;
+    console.log(date);
+    this.stream = this.cfurl + "media/" + camtype + "/" + date + "/output.m3u8";
+    //this.stream = 'https://guardisai-test-unit-2.bullshouse.in/media/Cashier22/20230803/output.m3u8';
+    console.log(this.stream);
+    this.vdata = { videolink: this.stream };
+    this.player.src({
+      src: this.stream,
+    });
+  }
 
-   
-  sanitize(url:string){
+  sanitize(url: string) {
     return this.sanitizer.bypassSecurityTrustUrl(url);
   }
-  checkHls(stream){
+  checkHls(stream) {
     if (Hls.isSupported()) {
-      var video = document.getElementById('sxmvideo');
+      var video = document.getElementById("sxmvideo");
       this.hls = new Hls();
       this.hls.attachMedia(this.video);
       this.hls.on(Hls.Events.MEDIA_ATTACHED, () => {
-        console.log('video and hls.js are now bound together !');
+        console.log("video and hls.js are now bound together !");
         this.hls.loadSource(stream);
-        this.hls.on(Hls.Events.MANIFEST_PARSED, function(event, data) {
+        this.hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
           console.log(
-            'manifest loaded, found ' + data.levels.length + ' quality level'
+            "manifest loaded, found " + data.levels.length + " quality level"
           );
         });
       });
@@ -95,11 +110,11 @@ export class LivestreamComponent implements OnInit,AfterViewInit {
           switch (data.type) {
             case Hls.ErrorTypes.NETWORK_ERROR:
               // try to recover network error
-              console.log('fatal network error encountered, try to recover');
+              console.log("fatal network error encountered, try to recover");
               this.hls.startLoad();
               break;
             case Hls.ErrorTypes.MEDIA_ERROR:
-              console.log('fatal media error encountered, try to recover');
+              console.log("fatal media error encountered, try to recover");
               this.hls.recoverMediaError();
               break;
             default:
@@ -112,19 +127,18 @@ export class LivestreamComponent implements OnInit,AfterViewInit {
     }
   }
   ngAfterViewInit() {
-    this.player = videojs(document.getElementById('sxmvideo'));
-      this.devicelist.map(function(value,index) {
-        videojs(document.getElementById('playbkvideo_'+value.device_id));
-        document.getElementById('playbkvideo_'+value.device_id).style.height = '130px';
-       });
-      this.player.muted(true);
+    this.player = videojs(document.getElementById("sxmvideo"));
+    this.devicelist.map(function (value, index) {
+      videojs(document.getElementById("playbkvideo_" + value.device_id));
+      document.getElementById("playbkvideo_" + value.device_id).style.height =
+        "130px";
+    });
+    this.player.muted(true);
   }
 
- 
-  playVideo(type){
+  playVideo(type) {
     const camType = type;
     this.cameraType = camType;
-    this.getPlayback(this.dateplay,camType);
+    this.getPlayback(this.dateplay, camType);
   }
- 
 }
